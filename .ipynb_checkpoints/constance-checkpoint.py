4a2179916a1c57880e5c35031b67971deaf1af40
Jakub Senk <jakub.senk@seznam.cz>
2023-08-01 02:18:08 +0200

    Remove checkpoints

--- .ipynb_checkpoints/constance-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# a dictionary holding different translations for "Table of Contents" under the keys of country codes
-table_of_contents = {'cz' : 'Obsah | Index',
-                     'en' : 'Table of Contents'}
-
-# a dictionary holding the tuples of main SFCR sections under the keys of country codes
-sfcr_sections = {'cz' : ('A. Činnost a výsledky | A. Činnost a výkonost | A – Činnost Kooperativy a její výkonnost | 1 Činnost a výsledky | A.  ČINNOST A VÝSLEDKY | Činnost ČPP a její výkonnost | Činnost a výsledky', 'B - Řídící a kontrolní systém | B. Řídící a kontrolní systém |  B – Řídící a kontrolní systém Kooperativy | 2 Řídící a kontrolní systém |  B – Řídící a kontrolní systém společnosti | Řídící a kontrolní systém' , 'C - Rizikový profil | 3 Rizikový profil | C.  RIZIKOVÝ PROFIL', 'D. Oceňování pro účely solventnosti | 4 Ocenění pro účely solventnosti', 'E. Řízení kapitálu | 5 Řízení kapitálu'),
-                 'en' : ('A. Business and Performance', 'B. Systems of Governance', 'C. Risk Profile', 'D. Valuation for Solvency Purposes', 'E. Capital Management')}
